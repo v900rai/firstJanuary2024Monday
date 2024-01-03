98b4201a7db2892b18f3e1fd4df382cdbd1b6530
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-03 17:42:01 +0530

    Add files via upload

---
+++ print_99_96_93.java
@@ -0,0 +1,10 @@
+package january.targettt50;
+
+public class print_99_96_93 {
+	public static void main(String []args) {
+		for(int i=99; i>=0; i=i-3) {
+			System.out.println(i+"..>");
+		}
+	}
+
+}
