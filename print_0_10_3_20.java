d54d77fb18a12efd6a4ab67d6a2a85bf21414a90
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-03 17:55:27 +0530

    Add files via upload

---
+++ print_0_10_3_20.java
@@ -0,0 +1,10 @@
+package january.targettt50;
+
+public class print_0_10_3_20 {
+	public static void main(String []args) {
+		for( int i=0, j=10; i<100 && j<100; i=i+3, j=j+10) {
+			System.out.print(i+".."+j+"..");
+		}
+	}
+
+}
