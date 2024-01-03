7f4551b4625462ed00be4e389b816807c4769b67
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-03 17:55:43 +0530

    Add files via upload

---
+++ print_2_4_8_16.java
@@ -0,0 +1,10 @@
+package january.targettt50;
+
+public class print_2_4_8_16 {
+	public static void main(String []args) {
+		for(int i=2; i<=100; i=i*i) {
+			System.out.print(i+"...>>>>>");
+		}
+	}
+
+}
