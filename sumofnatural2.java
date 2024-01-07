1f3f0cd042d5aa43e5a9c366b190e9182605d2bc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-07 23:04:54 +0530

    Add files via upload

---
+++ sumofnatural2.java
@@ -0,0 +1,17 @@
+package january.targettt50;
+
+public class sumofnatural2 {
+	public static void main(String[] args)   
+	{  
+	int i, num = 10, sum = 0;  
+	//executes until the condition returns true  
+	for(i = 1; i <= num; ++i)  
+	{  
+	//adding the value of i into sum variable  
+	sum = sum + i;  
+	}  
+	//prints the sum   
+	System.out.println("Sum of First 10 Natural Numbers is = " + sum+ " iii"+i);  
+	}  
+
+}
