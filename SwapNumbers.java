3541c8606aef78a6ccbfcf70e41b4bd8d874eeed
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-10 21:32:31 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,21 @@
+package january.targettt50;
+
+
+	public class SwapNumbers {
+
+		public static void main(String[] args) {
+			int a = 10;
+			int b = 20;
+
+		    System.out.println("a is " + a + " and b is " + b);
+
+			a = a + b;
+			b = a - b;
+			a = a - b;
+
+		    System.out.println("After swapping, a is " + a + " and b is " + b);
+		    }
+
+		}
+
+
