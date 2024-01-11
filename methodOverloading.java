e9f9993d55a01b588b65debdf173f559ff29efdf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-11 23:25:11 +0530

    Add files via upload

---
+++ methodOverloading.java
@@ -0,0 +1,13 @@
+package january.targettt50;
+
+public class methodOverloading {
+	static int add(int a,int b){return a+b;}  
+	static int add(int a,int b,int c){return a+b+c;}  
+	
+	 
+	public static void main(String[] args){  
+	System.out.println(methodOverloading.add(11,11));  
+	System.out.println(methodOverloading.add(11,11,11));  
+	}}  
+
+
