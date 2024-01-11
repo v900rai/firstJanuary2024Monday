7b4850849cb5bf7b6be1a3b21ac0a2d8babc994d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-11 23:30:49 +0530

    Add files via upload

---
+++ methodoverloadingtwo.java
@@ -0,0 +1,13 @@
+package january.targettt50;
+
+public class methodoverloadingtwo {
+	static int add(int a, int b){return a+b;}  
+	static double add(double a, double b){return a+b;}  
+	
+	  
+	public static void main(String[] args){  
+	System.out.println(methodoverloadingtwo.add(11,11));  
+	System.out.println(methodoverloadingtwo.add(12.3,12.6));  
+	}}  
+
+
