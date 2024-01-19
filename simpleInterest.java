3bf8f67a713c03ce805d38078d74b0af55d54d60
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-19 15:31:51 +0530

    Add files via upload

---
+++ simpleInterest.java
@@ -0,0 +1,11 @@
+package NumberProgram;
+
+public class simpleInterest {
+    public static void main(String []args){
+        float p=1, t=1 , r=1;
+        /* Calculate simple interest */
+        // calculate simple interest
+        float si= (p*r*t)/100;
+        System.out.println("Simple interest   ....>" +si);
+    }
+}
