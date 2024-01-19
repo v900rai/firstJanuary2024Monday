66d77c48380a42d66857664c89e61e4f5f70b436
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-19 15:29:16 +0530

    Add files via upload

---
+++ multiplicationOfTwoFloating.java
@@ -0,0 +1,11 @@
+package NumberProgram;
+
+public class multiplicationOfTwoFloating {
+    public static void main(String []args){
+        float f1= 1.2f;
+        float f2= 2.6f;
+        float f3= f1*f2;
+        System.out.println(f3+" ");
+
+    }
+}
