17f4dab44f81970c530262aeb564bce77cfbc27d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-22 19:41:28 +0530

    Add files via upload

---
+++ CountPunctuation.java
@@ -0,0 +1,18 @@
+package String;
+
+public class CountPunctuation {
+    public static void main(String args[]) {
+//Stores the count of punctuation marks
+        int count = 0;
+        String str = "He said, 'The mailman loves you.' I heard it with my own ears.";
+        for (int i = 0; i < str.length(); i++) {
+//Checks whether given character is punctuation mark
+            if (str.charAt(i) == '!' || str.charAt(i) == ',' || str.charAt(i) == ';' || str.charAt(i) == '.' || str.charAt(i) == '?' || str.charAt(i) == '-' ||
+                    str.charAt(i) == '\'' || str.charAt(i) == '\"' || str.charAt(i) == ':') {
+                count++;
+            }
+        }
+    }
+}
+
+
