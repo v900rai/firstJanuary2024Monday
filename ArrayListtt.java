113382aefdd68c0e09e5a7de9aff497a441c61a6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-23 12:25:39 +0530

    Add files via upload

---
+++ ArrayListtt.java
@@ -0,0 +1,16 @@
+package CollectionsFramework;
+import  java.util.ArrayList;
+public class ArrayListtt {
+    public static void main(String[] args) {
+        ArrayList <String> Name= new ArrayList<String>();
+        Name.add("Vishal");
+        Name.add("Ajay");
+        Name.add("arohi");
+        Name.add("rai");
+        System.out.println(Name);
+        Name.add("jai ram");
+        System.out.println(Name);
+        Name.add(0,"baijnath");
+        System.out.println(Name);
+    }
+}
