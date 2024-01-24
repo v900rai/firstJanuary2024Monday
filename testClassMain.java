7f588bbed89c23b5be20f9036d1c33ca33088683
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-24 18:51:19 +0530

    Add files via upload

---
+++ testClassMain.java
@@ -0,0 +1,13 @@
+package abstractInJava;
+
+import java.awt.*;
+
+public class testClassMain {
+    public static void main(String[] args) {
+       // Vehicle veh=new Vehicle(2);
+        car car=new car();
+        car.commute();
+        car.makeStartSound();
+
+    }
+}
