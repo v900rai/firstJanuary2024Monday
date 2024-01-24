7aa2665c86493b8d98e87bbdcd93b7fc31cb5f90
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-24 18:50:44 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,15 @@
+package abstractInJava;
+
+public class car  extends  Vehicle{
+    private int noOfDoors;
+
+
+    public car() {
+        super(4);
+    }
+
+    @Override
+    public void makeStartSound() {
+
+    }
+}
