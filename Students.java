eea05d25148913555461f593bef0558c374e9fe3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-01-26 18:01:10 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,14 @@
+package ClasseAndObject;
+
+public class Students {
+    int id;
+    String name;
+
+
+    public static void main(String[] args) {
+       Students st=new Students();
+       st.id=10;
+       st.name=" vishal";
+       System.out.println(st.id+" .."+st.name);// printing the element the decleard arrays
+    }
+}
