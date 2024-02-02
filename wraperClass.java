3d48d7bdd8eed57e8917a7b3d0d445a6f3766277
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-02 19:36:19 +0530

    Add files via upload

---
+++ wraperClass.java
@@ -0,0 +1,12 @@
+package NumberProgram;
+
+public class wraperClass {
+    public static void main(String[] args) {
+        Integer myInt = 5;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+    }
+}
