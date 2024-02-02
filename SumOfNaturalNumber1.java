4cc22608047f84ad6c3666add67882a603a32761
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-02 19:18:43 +0530

    Add files via upload

---
+++ SumOfNaturalNumber1.java
@@ -0,0 +1,12 @@
+package NumberProgram;
+
+public class SumOfNaturalNumber1 {
+    public static  void main(String []args){
+        int sum=0;
+        int num=10;
+        for(int i=1; i<=num; i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
