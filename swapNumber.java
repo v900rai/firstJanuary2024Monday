e6fded90a23afc8997d6bd61170af6603ada5203
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-03 21:14:58 +0530

    Add files via upload

---
+++ swapNumber.java
@@ -0,0 +1,17 @@
+package target200qQQQTTOOnnSS;
+
+public class swapNumber 
+{
+	public static void main(String []args)
+	{
+	int a=10;
+	int b=20;
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("a,,,"+a +"b...."+b);
+	
+	
+	}
+}
