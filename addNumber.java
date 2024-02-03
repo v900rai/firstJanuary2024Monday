6eaf20f3ea9544ab313b93dc566554e37384e1e6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-03 21:11:28 +0530

    Add files via upload

---
+++ addNumber.java
@@ -0,0 +1,13 @@
+package target200qQQQTTOOnnSS;
+
+public class addNumber 
+{
+	public static void main(String []args)
+	{
+		int a=20;
+		int b=30;
+		int result=a+b;
+		System.out.println(result);
+	}
+
+}
