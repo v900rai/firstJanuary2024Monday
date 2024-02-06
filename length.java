f2b9302feec22132ec47e96b4ad541ac8c069161
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-06 16:34:05 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,10 @@
+package StringInJava;
+
+public class length {
+    public static void main(String []args){
+        String str="Vishal Rai";
+        int l=str.length();
+        // find the length of the character in the given
+        System.out.println(l);
+    }
+}
